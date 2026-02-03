795a74402e516ab72ee483e6f18f9b3da7be2fec
David Soria Parra <davidsp@anthropic.com>
2026-02-03 15:45:30 +0000

    Add MAINTAINERS role to evalstate

--- src/config/users.ts
+++ src/config/users.ts
@@ -168,7 +168,12 @@ export const MEMBERS: readonly Member[] = [
   {
     github: 'evalstate',
     discord: '779268016121577492',
-    memberOf: [ROLE_IDS.COMMUNITY_MANAGERS, ROLE_IDS.DOCS_MAINTAINERS, ROLE_IDS.MODERATORS],
+    memberOf: [
+      ROLE_IDS.COMMUNITY_MANAGERS,
+      ROLE_IDS.DOCS_MAINTAINERS,
+      ROLE_IDS.MAINTAINERS,
+      ROLE_IDS.MODERATORS,
+    ],
   },
   {
     github: 'fabpot',
